851cbfbdc2a564b50282384cc2fa774ce3f3ef1c
Jay <jay@mymail.com>
2024-05-17 23:26:26 -0700

    Changed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ const App = () => {
   const [results, setResults] = useState('');
 
   const problemStatement = `
-  Write a function called 'matrix_multiply' that takes two matrices 'A' and 'B' (represented as lists of lists) 
-  and returns their product. If the matrices cannot be multiplied due to incompatible dimensions, the function should return 'None'.
-
+  A knight and a pawn are on a chess board. Can you figure out the minimum number of moves required for the knight to travel to the same position of the pawn? 
+  On a single move, the knight can move in an "L" shape; two spaces in any direction, then one space in a perpendicular direction. This means that on a single move, a knight has eight possible positions it can move to.
+  Write a function, knight_attack, that takes in 5 arguments:
+  n, kr, kc, pr, pc
+  n = the length of the chess board
+  kr = the starting row of the knight
+  kc = the starting column of the knight
+  pr = the row of the pawn
+  pc = the column of the pawn
+  The function should return a number representing the minimum number of moves required for the knight to land on top of the pawn. The knight cannot move out of bounds of the board. 
+  You can assume that rows and columns are 0-indexed. This means that if n = 8, there are 8 rows and 8 columns numbered 0 to 7. If it is not possible for the knight to attack the pawn, then return None.
+  
   Example:
-  - matrix_multiply([[1, 2], [3, 4]], [[5, 6], [7, 8]]) should return [[19, 22], [43, 50]]
-  - matrix_multiply([[1, 2]], [[3], [4]]) should return [[11]]
+  - knight_attack(8, 1, 1, 2, 2) should return 2
+  - knight_attack(8, 1, 1, 2, 3) should return 1
   `;
 
   const handleSubmit = async () => {
